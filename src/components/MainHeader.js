a18909dbdbfcd40f1a1e0cc8169cbfad1cd0c18f
taohui <htao@thoughtworks.com>
2022-09-23 09:19:04 +0800

    use link to do the redirection to avoid page refresh

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 const MainHeader = () => {
     //it will refresh the page
     return (<header>
         <ul>
             <li>
-                <a href="/welcome">Welcome</a>
+                <Link to="/welcome">Welcome</Link>
             </li>
             <li>
-                <a href="/products">Products</a>
+                <Link to="/products">Products</Link>
             </li>
         </ul>
     </header>)
