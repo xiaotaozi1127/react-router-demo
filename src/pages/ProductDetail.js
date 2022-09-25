f593de1e6163d8c9468288395239ce83b6f68e2e
taohui <htao@thoughtworks.com>
2022-09-25 14:58:21 +0800

    use useHistory to control programming navigation

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,11 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 const ProductDetail = () => {
     const params = useParams();
+    const history = useHistory();
+
+    const clickHandler = () => {
+        history.push("/products")
+    }
     return (
         <section>
             <h1>Product Detail</h1>
             <p>{params.productId}</p>
+            <button onClick={clickHandler}>Go Back</button>
         </section>
     )
 }
